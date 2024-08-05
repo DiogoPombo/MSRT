42d9f163a8f74f7390c1f27f5dee658773218117
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-08-04 21:06:00 -0300

    Add files via upload

---
+++ MSRT.bat
@@ -0,0 +1,13 @@
+@echo off
+
+start /realtime mrt.exe
+
+set APP=Windows Malicious Software Removal Tool
+set AUTHOR=POMBO
+set AVATAR=\Õ/
+set MADE_BY=MADE BY:
+set SPACE= 
+set KEY=(MSRT)
+echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
+
+exit
